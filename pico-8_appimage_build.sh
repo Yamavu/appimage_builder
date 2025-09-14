c4b675bf7286765ccb049e062e5c70ac61bd5bb0
Yamavu <gh@yamavu.com>
2025-09-14 14:03:20 +0200

    init

---
+++ pico-8_appimage_build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+base_name="pico-8"
+version="0.2.7"
+
+"amd64" "i386" "raspi"
+
+# pico-8_0.2.7_amd64.zip
+# pico-8_0.2.7_i386.zip
+# pico-8_0.2.7_raspi.zip
+
+# target: pico-8_0.2.7_amd64.appimage
